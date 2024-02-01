0393f4579eee86e06e2227d904c58dc46ce3a87c
Abigail  Nyameyie Arko <abigail.arko@amalitech.com>
2024-02-01 17:17:48 +0000

    login test finsh

---
+++ cypress/e2e/new.cy.js
@@ -0,0 +1,10 @@
+describe('template spec', () => {
+  it('passes', () => {
+  cy.visit("https://demo.opencart.com/index.php?route=account/register&language=en-gb") 
+  cy.get("#input-firstname").type("Abigail")
+  cy.get("#input-lastname").type("Arko")
+  cy.get("#input-email").type("abigial@gmail.com")
+  cy.get("#input-password").type("abigail22q2&Arko")
+  cy.get(".float-end > .btn").click()
+  })
+})
